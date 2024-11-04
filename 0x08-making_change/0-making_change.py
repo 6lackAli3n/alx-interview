c0e74a94e8f530d99a0063f0f0b766d4b6ee1902
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-11-04 02:04:28 +0100

    update

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -13,9 +13,9 @@ def makeChange(coins, total):
     change = 0
     coins = sorted(coins)[::-1]
     for coin in coins:
-    while coin <= total:
-    total -= coin
-    change += 1
-    if (total == 0):
-        return change
+        while coin <= total:
+            total -= coin
+            change += 1
+            if (total == 0):
+                return change
     return -1
